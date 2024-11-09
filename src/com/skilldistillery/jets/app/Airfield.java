7871086c426b529e0363b1ca6f695b7c81e45e8c
RaymondCostales <raymond.w.costales@gmail.com>
2024-11-09 11:48:20 -0700

    take information from txt

--- src/com/skilldistillery/jets/app/Airfield.java
+++ src/com/skilldistillery/jets/app/Airfield.java
@@ -1,12 +1,67 @@
 package com.skilldistillery.jets.app;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.skilldistillery.jets.entities.CargoJet;
+import com.skilldistillery.jets.entities.FighterJet;
 import com.skilldistillery.jets.entities.Jet;
+import com.skilldistillery.jets.entities.PassengerJet;
 
 public class Airfield {
 	
-	private List<Jet> fleet;
-	//NO other fields, just methods
+	private List<Jet> fleet = new ArrayList<>();
+
+	public Airfield() {
+		loadJetsFromFile("jets.txt");
+	}
+	private void loadJetsFromFile(String fileName) {
+
+		 try {
+			 BufferedReader bufIn = new BufferedReader(new FileReader(fileName));
+			 String line;
+			 while ((line = bufIn.readLine()) != null) {
+				 
+				 String[] jetData = line.split(", ");
+				 
+				 String jetType = jetData[0];
+				 String jetModel = jetData[1];
+				 int jetSpeed = Integer.parseInt(jetData[2]);
+			     int jetRange = Integer.parseInt(jetData[3]);
+				 double jetPrice = Double.parseDouble(jetData[4]);
+
+				if (jetType.equals("Cargo")) {
+					fleet.add(new CargoJet(jetModel, jetSpeed, jetRange, jetPrice));
+				}
+				else if (jetType.equals("Fighter")) {
+					fleet.add(new FighterJet(jetModel, jetSpeed, jetRange, jetPrice));
+				}
+				else if (jetType.equals("Passenger")) {
+					fleet.add(new PassengerJet(jetModel, jetSpeed, jetRange, jetPrice));
+				}
+				 
+			 }
+			}
+			catch (IOException e) {
+			  System.err.println(e);
+			}
+		 }
 
 }
+
+
+
+
+
+
+
+	//public methods for handling jets
+
+
+//				 Jet j = new Jet(jetType, jetModel, jetSpeed, jetRange, jetPrice);
+//				 fleet.add(j);
+//				 System.out.println(j);
+//				 System.out.println(line);
--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -24,10 +24,36 @@ public class JetsApplication {
 	
 	private void launch() {
 //		Loop: displayUserMenu(), get choice from scanner, and act on choice
+		displayUserMenu();
 	}
 	private void displayUserMenu() {
+		System.out.println("*******************AIRFIELD*******************");
+		System.out.println("1.List Fleet                    6.DOGFIGHT!!");
+		System.out.println("2.Fly All Jets                  7.Add A Jet");
+		System.out.println("3.View Fastest Jet              8.Remove A Jet");
+		System.out.println("4.View Jet With Longest Range   9.Quit");
+		System.out.println("5.Load All Cargo Jets");
+		System.out.println("**********************************************");
+	}
+	private void userChoice() {
+		System.out.println("Please enter a number");
+		int choice = kb.nextInt();
+		
+		
+		switch (choice) {
+		case 1:
+			listFleet();
+			break;
+		case 2:
+//			fly();
+			break;
+		case 3:
+			
+			
+		}
+	}
+		
 //		sysouts
 		
-	}
 
 }
